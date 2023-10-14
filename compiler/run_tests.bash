8b27c0726cbbb337c40d5b354eac86eb23e45012
Roger Burtonpatel <roger.burtonpatel@tufts.edu>
2023-10-14 11:28:41 -0400

    added tests- here you go jackson

--- compiler/run_tests.bash
+++ compiler/run_tests.bash
@@ -1,5 +1,12 @@
-#! /bin/bash 
+#!/bin/bash
 
-for testFile in $(ls tests) ; do 
-    echo "Running test: "$testFile
+for inFile in $(find tests -type f -iname "*.in") ; do
+    inBase=$(basename $inFile)
+    outFile="tests/${inBase%.*}.out"
+    [ ! -f $outFile ] && outFile=$inFile # if in = out, don't need out
+    echo "Running test: "$inFile $outFile
+    diff <(cat $inFile) $outFile
+    if [ "$?" -ne "0" ];  then
+        >&2 echo "Test $inFile $outFile failed above"
+    fi
 done
---
+++ compiler/tests/test.ml
@@ -0,0 +1,2 @@
+let%test "rev" =
+  List.equal Int.equal (List.rev [ 3; 2; 12 ]) [ 1; 2; 3 ]
