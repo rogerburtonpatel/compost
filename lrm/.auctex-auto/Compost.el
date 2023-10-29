ff85ed7f99f0769b4b5e478f9052b6f7f1d823fa
Jasper Geer <jasper.geer@gmail.com>
2023-10-28 20:31:22 -0400

    remove unnecessary files

---
+++ lrm/.auctex-auto/Compost.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "Compost"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "amssymb"
+    "amsmath"
+    "amsthm"
+    "fullpage"
+    "graphicx"
+    "caption"
+    "hyperref"
+    "listings"
+    "changepage"
+    "syntax")
+   (TeX-add-symbols
+    "op"
+    "cl"))
+ :latex)
+
