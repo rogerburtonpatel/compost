fd3bae2634482e8ef740a1a4d23a4fda67ae25b6
Jasper Geer <jasper.geer@gmail.com>
2023-11-01 18:01:30 -0400

    start on codegen

--- compiler/bin/codegen.ml
+++ compiler/bin/codegen.ml
@@ -1,3 +1,8 @@
 module L = Llvm
+module M = Mast
 
-let foo = L.create_context
+let ctx = L.create_context
+
+let rec expr builder functions locals ctx = function
+  | M.Literal l -> match l with
+    | _ -> ()
