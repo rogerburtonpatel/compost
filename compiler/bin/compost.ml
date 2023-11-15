b85278d5a1adb9b7e6776cdc49c7f189e3e8f08b
Jackson <11669560+jbwar22@users.noreply.github.com>
2023-11-14 20:06:45 -0500

    rename pre to past

--- compiler/bin/compost.ml
+++ compiler/bin/compost.ml
@@ -8,14 +8,14 @@ module M = Memorymanage
 module G = Codegen
 module Pre = Preprocess
 
-type action = Ast | Pre | UAst | TAst | MAst | Compile
+type action = Ast | Past | UAst | TAst | MAst | Compile
 
 let () =
   let action = ref Compile in
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast), "Print the AST");
-    ("-p", Arg.Unit (set_action Pre), "Preprocess & Print the AST");
+    ("-p", Arg.Unit (set_action Past), "Preprocess & Print the AST");
     ("-u", Arg.Unit (set_action UAst), "Print the UAST");
     ("-t", Arg.Unit (set_action TAst), "Typecheck and print UAst");
     ("-m", Arg.Unit (set_action MAst), "Typecheck, analyze consumption, add explicit memory management, and print MAst");
@@ -31,28 +31,28 @@ let () =
   match !action with
     Ast ->
     print_string (Ast.string_of_program ast)
-  | Pre ->
-    let pre = Pre.preprocess ast in
-    print_string (Past.string_of_program pre)
+  | Past ->
+    let past = Pre.preprocess ast in
+    print_string (Past.string_of_program past)
   | UAst ->
-    let pre = Pre.preprocess ast in
-    let uast = D.disambiguate pre in
+    let past = Pre.preprocess ast in
+    let uast = D.disambiguate past in
     print_string(Uast.string_of_program uast)
   | TAst ->
-    let pre = Pre.preprocess ast in
-    let uast = D.disambiguate pre in
+    let past = Pre.preprocess ast in
+    let uast = D.disambiguate past in
     let _ = Typecheck.typecheck uast in
     print_string (Uast.string_of_program uast)
   | MAst ->
-    let pre = Pre.preprocess ast in
-    let disambiguated = D.disambiguate pre in
+    let past = Pre.preprocess ast in
+    let disambiguated = D.disambiguate past in
     let (type_checked, _, _) = T.typecheck disambiguated in
     let consumption_checked = C.consumption_check type_checked in
     let memory_managed = M.mast_of_fast consumption_checked in 
     print_string (Mast.string_of_program memory_managed)
   | Compile ->
-    let pre = Pre.preprocess ast in
-    let disambiguated = D.disambiguate pre in
+    let past = Pre.preprocess ast in
+    let disambiguated = D.disambiguate past in
     let (type_checked, _, _) = T.typecheck disambiguated in
     let consumption_checked = C.consumption_check type_checked in
     let memory_managed = M.mast_of_fast consumption_checked in
